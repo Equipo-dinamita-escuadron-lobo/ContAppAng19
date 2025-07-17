a7658c5a3c7eaa9e8c45ea08809580ab9d677bb5
JulianRuano <julianrmajin@gmail.com>
2025-07-17 17:43:29 -0500

    Agregar componente 'view-enterprise' con su plantilla y estilos

---
+++ src/app/GeneralMasters/Enterprise/view-enterprise/view-enterprise.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-enterprise',
+  imports: [],
+  templateUrl: './view-enterprise.component.html',
+  styleUrl: './view-enterprise.component.css'
+})
+export class ViewEnterpriseComponent {
+
+}
