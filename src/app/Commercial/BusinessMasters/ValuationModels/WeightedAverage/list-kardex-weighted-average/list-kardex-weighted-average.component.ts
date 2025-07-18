ac55b62c39f49c6276a5fe84cbc37d7a77ac1ad2
JulianRuano <julianrmajin@gmail.com>
2025-07-18 15:30:09 -0500

    Add component 'list-kardex-weighted-average'

---
+++ src/app/Commercial/BusinessMasters/ValuationModels/WeightedAverage/list-kardex-weighted-average/list-kardex-weighted-average.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-kardex-weighted-average',
+  imports: [],
+  templateUrl: './list-kardex-weighted-average.component.html',
+  styleUrl: './list-kardex-weighted-average.component.css'
+})
+export class ListKardexWeightedAverageComponent {
+
+}
