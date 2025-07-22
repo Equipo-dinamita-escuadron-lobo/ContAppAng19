fd87b71f1d2ba4daa40272de412aee692fa9ed2f
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2025-07-21 20:34:59 -0500

    Added baseHref

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  baseHref: "/",
   keycloak_url: 'http://contables.unicauca.edu.co/',
   keycloak_url_token: 'http://contables.unicauca.edu.co/keycloak/token/',
   API_URL: 'http://contables.unicauca.edu.co/api/',
--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  baseHref: "/",
   keycloak_url: 'http://contables.unicauca.edu.co/',
   keycloak_url_token: 'http://contables.unicauca.edu.co/keycloak/token/',
   API_URL: 'http://localhost:8080/api/',
