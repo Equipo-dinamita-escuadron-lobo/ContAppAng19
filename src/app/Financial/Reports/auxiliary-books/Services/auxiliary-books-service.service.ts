8762a8bdb0a7703a5e24e61b2b462b911796b72d
AlejFernandezV <alejfernandez@unicauca.edu.co>
2025-10-13 19:13:33 -0500

    Fix: Change enviroment in Auxiliary Book service file

--- src/app/Financial/Reports/auxiliary-books/Services/auxiliary-books-service.service.ts
+++ src/app/Financial/Reports/auxiliary-books/Services/auxiliary-books-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../../environments/environment.local';
+import { environment } from '../../../../../environments/environment.dev';
 import { auxBookResponse } from '../Models/Responses/BookResponse';
 import { ExportAuxiliaryBookRequest } from '../Models/Requests/ExportAuxiliaryBookRequest';
 
