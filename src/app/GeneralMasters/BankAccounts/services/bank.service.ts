836b37f7b3a301378b76dbf97d3b6561f05fe532
santiagonieto09 <santiago.nieto09@gmail.com>
2025-10-24 09:17:41 -0500

    refactor: aplicar optional chaining.

--- src/app/GeneralMasters/BankAccounts/services/bank.service.ts
+++ src/app/GeneralMasters/BankAccounts/services/bank.service.ts
@@ -57,13 +57,13 @@ export class BankService {
   findAll(enterpriseId: string, page: number = 0, size: number = 10, sortField?: string, sortOrder?: string, search?: string): Observable<PageResponse<Bank>> {
     let url = `${this.API_BASE}/findAll/${enterpriseId}?page=${page}&size=${size}`;
 
-    if (sortField && sortField.trim()) {
+    if (sortField?.trim()) {
       url += `&sortField=${sortField}`;
     }
-    if (sortOrder && sortOrder.trim()) {
+    if (sortOrder?.trim()) {
       url += `&sortOrder=${sortOrder}`;
     }
-    if (search && search.trim()) {
+    if (search?.trim()) {
       url += `&search=${encodeURIComponent(search)}`;
     }
 
