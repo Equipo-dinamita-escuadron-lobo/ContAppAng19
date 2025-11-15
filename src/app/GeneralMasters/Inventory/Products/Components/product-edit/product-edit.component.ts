4cab559f4b5692b6c2d1760abc157bcda4c439cb
santiagonieto09 <santiago.nieto09@gmail.com>
2025-11-15 14:14:39 -0500

    refactor: Mostrar mensaje uso editar producto

--- src/app/GeneralMasters/Inventory/Products/Components/product-edit/product-edit.component.ts
+++ src/app/GeneralMasters/Inventory/Products/Components/product-edit/product-edit.component.ts
@@ -195,12 +195,23 @@ export class ProductEditComponent implements OnInit {
       },
       error: (err) => {
         console.error('Error al actualizar el producto:', err);
-        const errorMessage = err?.error?.message || 'Ha ocurrido un problema al actualizar el producto.';
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: errorMessage
-        });
+
+        // Verificar si el error específico de producto en uso
+        const errorCode = err?.error?.code || err?.code || '';
+        if (errorCode === 'PRODUCT_IN_USE') {
+          this.messageService.add({
+            severity: 'info',
+            summary: 'Información',
+            detail: err?.error?.message || 'No se puede editar el producto porque tiene movimientos contables'
+          });
+        } else {
+          const errorMessage = err?.error?.message || 'Ha ocurrido un problema al actualizar el producto.';
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: errorMessage
+          });
+        }
       }
     });
   }
