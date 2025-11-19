7a38329ee7910857e69637a63871f0deec932823
santiagonieto09 <santiago.nieto09@gmail.com>
2025-11-18 21:35:08 -0500

    refactor: mostrar usagecount en response centro de costo

--- src/app/GeneralMasters/CostCenters/models/cost-center.model.ts
+++ src/app/GeneralMasters/CostCenters/models/cost-center.model.ts
@@ -1,15 +1,17 @@
 export interface CostCenter {
   id?: number;
   idEnterprise: string;
-  code: string; 
+  code: string;
   name: string;
   parentId?: number | null;
   status?: boolean;
+  usageCount?: number;
 }
 
 export interface CostCenterNode extends CostCenter {
   children?: CostCenterNode[];
   showChildren?: boolean;
+  usageCount?: number;
 }
 
 
